d54992478018e36e2bd73bcb7ddbd15edef533a1
jongwoop97@gmail.com <jongwoop97@gmail.com>
2023-11-16 15:31:46 +0900

    initial commit

---
+++ src/main/java/com/ssafy/la/LoginAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.la;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAdventureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/la/LoginAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.la;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAdventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
