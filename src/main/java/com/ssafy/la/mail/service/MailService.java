2ecf7f1e8cd7697e53be8f6c0cb4c091a96355dc
Shinwooseob <dntjq0807@naver.com>
2023-11-17 11:50:49 +0900

    fix: 어노테이션 추가

--- src/main/java/com/ssafy/la/mail/service/MailService.java
+++ src/main/java/com/ssafy/la/mail/service/MailService.java
@@ -1,5 +1,8 @@
 package com.ssafy.la.mail.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MailService {
 
 }
--- src/main/java/com/ssafy/la/util/security/SecurityUtil.java
+++ src/main/java/com/ssafy/la/util/security/SecurityUtil.java
@@ -1,5 +1,8 @@
 package com.ssafy.la.util.security;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SecurityUtil {
 
 }
