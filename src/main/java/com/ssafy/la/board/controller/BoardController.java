100f28be595275feb5aa9e7dcc8adc3138b3f00d
jongwoop97@gmail.com <jongwoop97@gmail.com>
2023-11-17 14:11:29 +0900

    fix: controller annnotation restcontroller로 변경

--- src/main/java/com/ssafy/la/board/controller/BoardController.java
+++ src/main/java/com/ssafy/la/board/controller/BoardController.java
@@ -1,5 +1,8 @@
 package com.ssafy.la.board.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BoardController {
 
 }
--- src/main/java/com/ssafy/la/user/controller/UserController.java
+++ src/main/java/com/ssafy/la/user/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.ssafy.la.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.la.user.model.service.UserService;
 
-@Controller
+@RestController
 public class UserController {
 	
 	@Autowired
