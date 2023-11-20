67c79c8275b8b17874b3e8e8014bd77c79312058
Shinwooseob <dntjq0807@naver.com>
2023-11-20 09:55:36 +0900

    feat: Redis에 expireTime을 설정한 save 메소드, delete 메소드 추가

--- src/main/java/com/ssafy/la/user/model/dao/UserRedisDao.java
+++ src/main/java/com/ssafy/la/user/model/dao/UserRedisDao.java
@@ -1,5 +1,7 @@
 package com.ssafy.la.user.model.dao;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -9,12 +11,23 @@ public class UserRedisDao {
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
-
+    
+    /*
+     * Duration.ofMillis(Long tokenLive)
+     */
+    public void saveToRedis(String key, String value, Duration duration) {
+    	redisTemplate.opsForValue().set(key, value, duration);
+    }
+    
     public void saveToRedis(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
     }
 
     public String readFromRedis(String key) {
         return redisTemplate.opsForValue().get(key);
+    }
+    
+    public void deleteFromRedis(String key) {
+    	redisTemplate.delete(key);
     }
 }
