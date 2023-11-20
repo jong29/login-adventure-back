9e4cc31c624a52b37ab04b51c2a82186cc2ad1a2
신우섭 <dntjq0807@naver.com>
2023-11-20 13:10:13 +0900

    Rsa 및 Filter

--- src/main/java/com/ssafy/la/filter/GlobalFilter.java
+++ src/main/java/com/ssafy/la/filter/GlobalFilter.java
@@ -1,5 +1,6 @@
 package com.ssafy.la.filter;
 
+import com.ssafy.la.user.model.dao.UserRedisDao;
 import com.ssafy.la.user.model.security.dao.SecurityMapper;
 import com.ssafy.la.util.exception.exceptions.MyException;
 import com.ssafy.la.util.security.JWTProvider;
@@ -18,50 +19,74 @@ import java.util.List;
 @Component
 public class GlobalFilter extends OncePerRequestFilter {
 
-    @Autowired
-    JWTProvider jwtProvider;
+	@Autowired
+	JWTProvider jwtProvider;
 
-    @Autowired
-    SecurityMapper securityMapper;
+	@Autowired
+	SecurityMapper securityMapper;
 
-    private final List<String> whiteList = new ArrayList<>(Arrays.asList("login", "logout", "modify", "delete"));
+	@Autowired
+	UserRedisDao userRedisDao;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String requestURI = request.getRequestURI();
-        String[] splitURI = requestURI.split("/");
-        switch (splitURI[1]) {
-            case "user":
-            case "board":
-                if (checkAuthorization(splitURI[2])) {
-                    String salt = null;
-                    if ("reissueAtk".equals(splitURI[2])) {
-                        String rtk = (String) request.getAttribute("rtk");
-                        if (rtk == null) {
-                            throw new MyException();
-                        }
-                        if (!jwtProvider.validateToken(rtk, salt)) {
-                            throw new MyException();
-                        }
-                    }
-                    String atk = (String) request.getAttribute("atk");
-                    if (atk == null) {
-                        throw new MyException();
-                    }
-//                    salt = securityMapper.getSalt();
-                    if (!jwtProvider.validateToken(atk, salt)) {
-                        throw new MyException();
-                    }
-                }
-                break;
-            default:
-                throw new MyException();
-        }
+	private final List<String> whiteList = new ArrayList<>(Arrays.asList("login", "logout", "modify", "delete"));
 
-        filterChain.doFilter(request, response);
-    }
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String requestURI = request.getRequestURI();
+		String[] splitURI = requestURI.split("/");
+		switch (splitURI[1]) {
+		case "user":
+		case "board":
+			if (checkAuthorization(splitURI[2])) { // 권한이 필요한 상황
+				String salt = null;
+				String userid = (String) request.getAttribute("userid");
 
-    private boolean checkAuthorization(String request) {
-        return whiteList.contains(request);
-    }
+				if (userid == null) {
+					throw new MyException();
+				}
+
+				if ("reissue".equals(splitURI[2])) { // 토큰 재발급 상황
+					String rtk = (String) request.getAttribute("rtk");
+					if (rtk == null) { // rtk가 없는 경우
+						throw new MyException();
+					}
+					String token = userRedisDao.readFromRedis("rtk:" + userid);
+					if (token == null) {
+						throw new MyException();
+					}
+					if (rtk.equals(token)) {
+//						salt = securityMapper.getSalt(userid);	// salt값 가져와서 한 번 더 토큰 검증
+						if (!jwtProvider.validateToken(rtk, salt)) {
+							throw new MyException();
+						}
+					}
+
+				}
+				String atk = (String) request.getAttribute("atk");
+				if (atk == null) { // atk이 없는 경우
+					throw new MyException();
+				}
+				String token = userRedisDao.readFromRedis("atk:" + userid);
+				if (token == null) {
+					throw new MyException();
+				}
+				if (atk.equals(token)) {
+//					salt = securityMapper.getSalt(userid);
+					if (!jwtProvider.validateToken(atk, salt)) { // atk 만료된 상황 -> rtk 들고 오라고 응답
+						throw new MyException();
+					}
+				}
+			}
+			break;
+		default:
+			throw new MyException();
+		}
+
+		filterChain.doFilter(request, response);
+	}
+
+	private boolean checkAuthorization(String request) {
+		return whiteList.contains(request);
+	}
 }
