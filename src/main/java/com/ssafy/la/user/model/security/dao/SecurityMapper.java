f1cef651ab544480cb47147fb02981b8f9c4f1f9
Shinwooseob <dntjq0807@naver.com>
2023-11-20 13:32:15 +0900

    feat: userMapper, SecurityMapper 구현

--- src/main/java/com/ssafy/la/user/model/security/dao/SecurityMapper.java
+++ src/main/java/com/ssafy/la/user/model/security/dao/SecurityMapper.java
@@ -1,7 +1,12 @@
 package com.ssafy.la.user.model.security.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface SecurityMapper {
+	
+	void insertSecurity(@Param("userid") String userid, @Param("salt") String salt);
+	
+	String readSalt(String userid);
 }
